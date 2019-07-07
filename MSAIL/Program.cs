315c6e81074df4db9abcd28baf2ac03b4acafd05
T3CHN01200 <1sohiervic@gmail.com>
2019-07-07 17:33:31 -0400

    First Commit: T3CHN01200 1sohiervic@gmail.com
    Introduced:
    -Retroactive logging
    -Log Pull Requests
    -Live Logging
    
    Additional Information:
    Logs all the text, file links and embeds
    Files can be pulled via:
    !pull - pulls file of channel requested in via DM
    !pull <#channel id> - pulls file of requested channel via DM
    !pull all - pulls zip file of current server via DM

---
+++ MSAIL/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+/*
+ * Authored by: T3CHN01200
+ * Discord: T3CHN01200#7178
+ * Last updated: 2019/7/7
+ */
+
+namespace MSAIL
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+
+        }
+
+        public async Task MainAsync()
+        {
+
+            Bot bot = new Bot();
+
+            await Task.Delay(-1);
+
+        }
+
+    }
+
+}
